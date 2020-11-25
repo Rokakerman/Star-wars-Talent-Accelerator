ab5f7414a83a3df98227733fad85c03561dda000
Jensengbg-Henrik-Johansson <54744154+Rokakerman@users.noreply.github.com>
2020-11-25 19:26:14 +0100

    working on infinite scroll

--- mystarwars/src/components/CharacterList.vue
+++ mystarwars/src/components/CharacterList.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-      
-      <ul>
+      <ul @scroll="scrolledToBottom" class="character-list">
           <li v-for="item in parentList" :key="item.name"> 
               <p> {{ item.name }} </p>
           </li>
@@ -18,10 +17,30 @@ export default {
     },
     props: {
         parentList: Array
+    },
+    methods: {
+        loadMore(event) {
+
+        },
+        scrolledToBottom(event){
+        var el = event.srcElement
+        console.log(el.scrollTop + " " + el.scrollHeight + " " + el.clientHeight)
+        if(!this.reachedBottom){
+            if(el.scrollTop >= (el.scrollHeight - el.clientHeight)-100){
+                this.reachedBottom = true
+            }
+        } 
+    },
     }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.character-list {
+    overflow: auto;
+    height: 9rem;
+    margin: 0px;
+    padding: 0px;
+}
 </style>
+
--- mystarwars/src/components/Search.vue
+++ mystarwars/src/components/Search.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <button @keyup="filterMe">  filter </button>
+        <button @click="filterMe">  filter </button>
         <input type="text" v-model="param" @keyup="filterMe">
     </div>
 </template>
@@ -16,10 +16,22 @@ export default {
     },
     methods: {
         async filterMe() {
-            const response = await fetch(this.URL + this.param , {method: 'GET', headers: {'Content-Type': 'application/json'}})
+            /*const response = await fetch(this.URL + this.param , {method: 'GET', headers: {'Content-Type': 'application/json'}})
             let readable = await response.json()
-            this.array = readable
-            console.log(readable)
+            let people = readable.results
+            let firstName = await people.filter(el => el.name.split(' ').slice(0, -1).join(' '))
+            console.log(firstName)
+            this.array = readable*/
+
+            const response = await fetch('http://swapi.dev/api/people/?page=1', {method: 'GET', headers: {'Content-Type': 'application/json'}})
+            let x = await response.json()
+            let list = x.results
+            console.log(list)
+
+            
+
+            /*let firstName = fullName.split(' ').slice(0, -1).join(' ');
+            let lastName = fullName.split(' ').slice(-1).join(' ');*/
 
             //let criteria = " o";
             //let results = this.array.filter(el => el.name.toLowerCase().includes(criteria.toLowerCase()))
